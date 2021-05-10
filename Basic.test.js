0a55682b50f5949609cd1f67f7eaa29227c4a61c
Mr Martin <1879990@students.wits.ac.za>
2021-05-10 14:39:21 +0200

    Testing

--- Basic.test.js
+++ Basic.test.js
@@ -2,3 +2,4 @@
 const BasicT = require('./Basic');
 test('When checkbox is checked',()=>
 {expect(BasicT()).toBe('<input name="bcoMark" type="number" id="bcomark" class="form-control"  min="0" max="100" required="required">');});
+ 
